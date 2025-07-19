2718e49fcac0fb0aea8580a62323661d20971ef7
Arnav-lunatic <arnav4c@gmail.com>
2025-07-19 21:03:03 +0530

    .

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const port = process.env.PORT || 8000;
 app.use("/api/videos", ytVideo);
 
 app.listen(port, () => {
-	console.log(`listening at http://localhost:${port}`);
+	console.log(`listening at PORT: ${port}`);
 });
